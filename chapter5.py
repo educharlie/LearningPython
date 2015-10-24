6666c6ea1b72c5741352d0bebd50aaed5d805b78
Carlos Fuentes <educharlief@hotmail.com>
2015-10-24 10:22:16 -0500

    testing chapter 5

---
+++ chapter5.py
@@ -0,0 +1,23 @@
+'''
+def test():
+	"""Do Something
+	"""
+
+	for x in xrange(1,10):
+		for y in xrange(1,6):
+			print str(x) + " " + str(y)
+
+test()
+
+print 1%2
+x=3
+
+if x<2:
+	print "si"
+elif x>=2 and x<=5:
+	print "talvez"
+else:
+ 	print "no"	
+'''
+name = raw_input("Cual es tu nombre?")
+print name
