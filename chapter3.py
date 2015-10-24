16a00c3fe631fd79cc86b037957b088281562331
Carlos Fuentes <educharlief@hotmail.com>
2015-10-24 09:34:08 -0500

    Adding new tests

---
+++ chapter3.py
@@ -0,0 +1,19 @@
+
+print "Goodbye, World!. I'm testing you"
+print type('3')
+print 1,000,000
+zipcode = 0232
+print zipcode
+print 01,010,0100
+print 01000
+minute = 1
+print minute /2 #floor division (when both operators are integers, the result is an integer)
+minute = 1.0
+print minute/2
+print "Hello" + " World"
+print "C4" * 4
+
+r= 5
+sphereVolume = (4.0/3.0)*3.14*r**3
+print sphereVolume
+print ggg
---
+++ chapter4.py
@@ -0,0 +1,28 @@
+ #coding=utf-8
+'''
+number = int('32')
+print number
+number = int(1.24)
+print number
+number = float('1.23')
+print number
+number = str(23)
+print number
+print type(number)
+'''
+'''
+import math
+radians = math.sin(0.7)
+print radians
+x = math.exp(1/360.0 * 2 * math.pi)
+print (x)
+'''
+def getSum(a,b):
+	print "Esta es una función: " + str(a) + " " + str(b)
+	print "El resultado es: " + str(a+b)
+
+number1 = 3
+number2 = 4
+getSum(number1, number2)
+print number1
+
